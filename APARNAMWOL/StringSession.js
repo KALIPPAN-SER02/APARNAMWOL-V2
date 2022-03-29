285ae7635dcd0f6b5d61d58f678e85a23a024857
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-03-30 02:36:50 +0530

    Update StringSession.js

--- APARNAMWOL/StringSession.js
+++ APARNAMWOL/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('AMALSER_CODE' in process.env && string === undefined) {
+        if ('APARNA_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Kalippanser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
