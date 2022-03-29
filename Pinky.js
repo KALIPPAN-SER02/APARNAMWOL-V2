241f41d9ae023c6d68f50203ed3f389d76d06997
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-03-30 02:26:09 +0530

    Update Pinky.js

--- Pinky.js
+++ Pinky.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+919895828468' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amalser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+918281370025' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Kalippanser' : process.env.DEPLOYER,    
 };
