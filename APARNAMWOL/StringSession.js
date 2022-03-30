1e7dac99e0d8bfe13fac31a393f30ecbabc49a75
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-03-30 11:25:55 +0530

    Update StringSession.js

--- APARNAMWOL/StringSession.js
+++ APARNAMWOL/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Kalippanser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Amalser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
