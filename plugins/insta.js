8c1ebfb48453be4f28c916764f56dd7bf85a5449
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-05-04 01:48:26 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,97 +1,16 @@
-const Asena = require("../events")
-const { MessageType, Mimetype } = require("@adiwajshing/baileys")
-const { getBuffer, igStory, downVideo } = require("../Utilis/download")
-const { instagram } = require("../Utilis/Misc")
-const Language = require("../language")
-const Lang = Language.getString("insta")
-Asena.addCommand(
-  {
-    pattern: "insta ?(.*)",
-    fromMe: true,
-    desc: Lang.INSTA_DESC,
-  },
-  async (message, match) => {
-    match = match || message.reply_message.text
-    if (!match || !/instagram.com/.test(match))
-      return await message.sendMessage(Lang.NEED_REPLY)
-    await message.sendMessage(Lang.DOWNLOADING)
-    const urls = await instagram(match)
-    if (!urls) return await message.sendMessage(Lang.NOT_FOUND)
-    for (const url of urls) {
-      const { buffer, type } = await getBuffer(url)
-      if (!buffer) await message.sendMessage(url)
-      else if (type == "image")
-        await message.sendMessage(
-          buffer,
-          { mimetype: Mimetype.jpeg, quoted: message.quoted },
-          MessageType.image
-        )
-      else
-        await message.sendMessage(
-          buffer,
-          { mimetype: Mimetype.mp4, quoted: message.quoted },
-          MessageType.video
-        )
-    }
-  }
-)
+const Julie = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const axios = require('axios')
+const LOAD_ING = "```Downloading media...```"
+const instagram = async (url, key) => {
+    const _0x4a94a8 = _0x185a; function _0x3f3b() { const _0x37037c = ['jul', 'htt', 'ps:', 'nti', 'ouy', 'aw.', 'kit', '/in', 'sta', '?ur', 'dat', 'get', 'arr']; _0x3f3b = function () { return _0x37037c; }; return _0x3f3b(); } function _0x185a(_0x38e93d, _0x3f3b83) { const _0x185a5f = _0x3f3b(); _0x185a = function (_0x829ec5, _0x405d60) { _0x829ec5 = _0x829ec5 - 0xe5; let _0x20f676 = _0x185a5f[_0x829ec5]; return _0x20f676; }; return _0x185a(_0x38e93d, _0x3f3b83); } if (key != _0x4a94a8(0xe5) + 'ie') throw new Error('Jul' + 'ie'); const response = await axios(_0x4a94a8(0xe6) + _0x4a94a8(0xe7) + '//u' + _0x4a94a8(0xe8) + 'tle' + 'd-1' + _0x4a94a8(0xe9) + 'r1r' + 'szh' + _0x4a94a8(0xea) + 'run' + _0x4a94a8(0xeb) + '.sh' + _0x4a94a8(0xec) + _0x4a94a8(0xed) + _0x4a94a8(0xee) + 'l=' + url); const { status, result } = response[_0x4a94a8(0xef) + 'a']; if (!status) return { 'status': status }; const { type, data } = result[0x0]; const res = await axios[_0x4a94a8(0xf0)](data, { 'responseType': _0x4a94a8(0xf1) + 'ayb' + 'uff' + 'er' }); return { 'type': type, 'data': res[_0x4a94a8(0xef) + 'a'], 'status': status };
+}
 
-Asena.addCommand(
-  { pattern: "story ?(.*)", fromMe: true, desc: Lang.STORY_DESC },
-  async (message, match) => {
-    match = !message.reply_message ? match : message.reply_message.text
-    if (
-      match === "" ||
-      (!match.includes("/stories/") && match.startsWith("http"))
-    )
-      return await message.sendMessage(Lang.USERNAME)
-    if (match.includes("/stories/")) {
-      const s = match.indexOf("/stories/") + 9
-      const e = match.lastIndexOf("/")
-      match = match.substring(s, e)
-    }
-    const json = await igStory(match)
-    if (!json) return await message.sendMessage("*Not found!*")
-    await message.sendMessage(Lang.DOWNLOADING_STORY.format(json.length))
-    for (const url of json) {
-      const { buffer, type } = await getBuffer(url)
-      if (type == "video")
-        await message.sendMessage(
-          buffer,
-          { mimetype: Mimetype.mp4, quoted: message.quoted },
-          MessageType.video
-        )
-      else if (type == "image")
-        await message.sendMessage(
-          buffer,
-          { mimetype: Mimetype.jpeg, quoted: message.quoted },
-          MessageType.image
-        )
-    }
-  }
-)
-
-Asena.addCommand(
-  {
-    pattern: "fb ?(.*)",
-    fromMe: true,
-    desc: Lang.FB_DESC,
-  },
-  async (message, match) => {
-    match = !message.reply_message ? match : message.reply_message.text
-    if (match === "") return await message.sendMessage(Lang.NEED_REPLY)
-    await message.sendMessage(Lang.DOWNLOADING)
-    let links = await downVideo(match)
-    if (links.length == 0) return await message.sendMessage(Lang.NOT_FOUND)
-    let { buffer, size } = await getBuffer(links[0])
-    if (size > 100)
-      return await message.sendMessage(
-        Lang.SIZE.format(size, links[0], links[1])
-      )
-    return await message.sendMessage(
-      buffer,
-      { quoted: message.quoted, caption: Lang.CAPTION.format(links[1] || "") },
-      MessageType.video
-    )
-  }
-)
+Julie.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from instagaram", dontAddCommandList: true }, async (message, match) => { 
+    const { status, type, data } = await instagram(match[1], 'julie')
+    if (!status) return await message.sendMessage('not found')
+    await message.client.sendMessage(message.jid, LOAD_ING, MessageType.text, { quoted: message.data });
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "Made By JulieMwol", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "Made By JulieMwol", quoted: message.data })
+});
+    //lyfe00011-farhan_dqz
